7b9aca12164e84727f997f829994bf0b0a73b454
saraheagle8 <93741599+saraheagle8@users.noreply.github.com>
2023-02-23 14:43:12 -0600

    2 files modified

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,19 +1,33 @@
 import React from 'react';
-
-var skillnames = ['Java', 'Javascript', 'React', 'Databases'];
-var images = ['java.png', 'python.png', 'react.png', 'sql.png'];
+var images = [
+  'https://ctechhosting.com/images-354/java.png',
+  'https://ctechhosting.com/images-354/python.png',
+  'https://ctechhosting.com/images-354/react.png',
+  'https://ctechhosting.com/images-354/sql.png',
+];
 
 export default function Skills(props) {
-  <div className="container">
-    <div className="row">
-      <div className="col">
-        <img src="???" width="75" height="75" />
-        <h4>Java</h4>
-        <p>
-          List skills technologies here. Aenean commodo ligula eget dolor.
-          Aenean commodo ligula eget dolor. Aenean commodo ligula eget dolor.
-        </p>
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          {/* <img src={images[0]} width="75" height="75" /> */}
+          images.map((i) => {
+            <img src={i} width="75" height="75" />;
+          });
+          <h4>{props.skillnames[1]}</h4>
+          <p>
+            List skills technologies here. Aenean commodo ligula eget dolor.
+            Aenean commodo ligula eget dolor. Aenean commodo ligula eget dolor.
+          </p>
+        </div>
       </div>
     </div>
-  </div>;
+  );
+}
+
+function Image({ images }) {
+  return images.map((i) => {
+    <img src={i} width="75" height="75" />;
+  });
 }
--- src/index.js
+++ src/index.js
@@ -5,6 +5,15 @@ import App from './App';
 import Nav from './components/Nav';
 import Intro from './components/Intro';
 import BlogPost from './components/BlogPost';
+import Skills from './components/Skills';
+
+var skillnames = ['Java', 'Javascript', 'React', 'Databases'];
+var images = [
+  'https://ctechhosting.com/images-354/java.png',
+  'python.png',
+  'react.png',
+  'sql.png',
+];
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
@@ -14,5 +23,6 @@ root.render(
     <Nav />
     <Intro />
     <BlogPost />
+    <Skills images={images} skillnames={skillnames} />
   </StrictMode>
 );
