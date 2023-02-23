0592d94c9cdcec7e9d2e940d57a09408da71abc2
saraheagle8 <93741599+saraheagle8@users.noreply.github.com>
2023-02-23 15:58:41 -0600

    1 file modified

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-import App from './App';
 import Nav from './components/Nav';
 import Intro from './components/Intro';
 import BlogPost from './components/BlogPost';
